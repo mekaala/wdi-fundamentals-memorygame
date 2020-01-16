4d5b558aa1fd9984141c73da543182cb19c6542b
Mekaal Ahmad <mekaalahmad@Mekaals-MacBook-Pro.local>
2020-01-16 18:57:53 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+const cardOne = cards[0];
+const cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) { 
+if(cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+}
