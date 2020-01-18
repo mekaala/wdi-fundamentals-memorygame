f82b0ad75d97d57d30a5ea51f404e683779ebdef
Mekaal Ahmad <mekaalahmad@Mekaals-MacBook-Pro.local>
2020-01-18 18:34:52 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,17 +27,31 @@ function checkForMatch() {
   alert("You found a match!");
 } else {
   alert("Sorry, try again.");
-}
+};
 }
 
-function flipCard(cardId) {
-console.log("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-if (cardsInPlay.length === 2) { 
-checkForMatch();
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) { 
+	checkForMatch();
+	};
 }
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementsByTagName('div')[0].appendChild(cardElement);
+	};
+
 }
-flipCard(0);
-flipCard(2);
+
+createBoard();
+
